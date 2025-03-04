d49cbf3b9f1c4d5c9ed905563e2059e51fbb9443
Siyabonga Mbuyisa <siyabongambuyisa7@gmail.com>
2025-03-04 20:25:54 +0200

    SignIn and SignUp Backend
    Users are able to sign up using username and password
    Users are able to sign in using those detail
    Next we need useres to sign, modify profile and fix frontend

--- ml_trader_project/accounts/views.py
+++ ml_trader_project/accounts/views.py
@@ -2,8 +2,9 @@
 
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.shortcuts import redirect
+from django.contrib import messages
 
 def signup_view(request):
     if request.method == 'POST':
@@ -11,24 +12,27 @@ def signup_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('home')
+            return redirect('login')
     else:
         form = UserCreationForm()
 
     return render(request, 'accounts/signup.html',  {'form': form})
 
 def login_view(request):
+  
     if request.method == 'POST':
-        # form = AuthenticationForm(data=request.POST)
-        # if form.is_valid():
-        #     user = form.get_user()
-        #     login(request, user)
-            return redirect('home')
-    # else:
-    #     form = AuthenticationForm()
-    return render(request, 'accounts/login.html')
+        username = request.POST["usernameInput"]
+        password = request.POST["passwordInput"]
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render('signup')
+        else:
+            messages.error(request, "Unsuccessful login. Please check your email and password and try again.")
+     
+    return render(request, 'accounts/login.html', {})
 
 def logout_view(request):
-    if request.method == 'POST':
-        logout(request)
-        return redirect('home')
+    #if request.method == 'POST':
+    logout(request)
+    return render(request, 'accounts/login.html', {})
