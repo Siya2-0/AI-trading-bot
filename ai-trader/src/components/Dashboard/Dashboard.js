26e90547344494d57e6a98089e485503e15cf4be
Siyabonga Mbuyisa <siyabongambuyisa7@gmail.com>
2025-03-20 23:09:00 +0200

    User Details Section + Dashboard Content
    Basic Structure of User Detail Section and Dashboard content

--- ai-trader/src/components/Dashboard/Dashboard.js
+++ ai-trader/src/components/Dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Dashboard.css";
-
+import defaultProfileImage from '../../assets/images/307ce493-b254-4b2d-8ba4-d12c080d6651.jpg';
 
 const Dashboard = ({ isCollapsed }) => {
 
@@ -8,7 +8,40 @@ const Dashboard = ({ isCollapsed }) => {
     return (
         <>
         <div className={`dashboard ${isCollapsed ? "collapsed" : ""}`} >
-            hello
+          
+            <div className="user-detail-container">
+                <div className='greeting-text'>
+                    <p>Good day, Siyabonga Mbuyisa!</p>
+                </div>
+                <div className='profile'>
+                    <i class="fa fa-bell bell-icon" aria-hidden="true"></i>
+                    <img className='profileImage' src={defaultProfileImage} alt='profileImage'></img>
+                    
+                </div>
+            </div>
+            <div className='dashboard-content'>
+                <div className='balance-container'>
+
+                </div>
+                <div className='broker-container'>
+
+                </div>
+                <div className='open-trades-container'>
+
+                </div>
+                <div className='closed-trades-container'>
+
+                </div>
+
+                <div className='chart-container'>
+
+                </div>
+                <div className='news-events-container'>
+
+                </div>
+
+            </div>
+
         </div>
         </>
     );
