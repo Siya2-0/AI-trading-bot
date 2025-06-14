32be6e5ce0a79c8a96f2c5b970010530654aba2c
Siyabonga Mbuyisa <siyabongambuyisa7@gmail.com>
2025-06-14 20:34:14 +0200

    Trader Body added
    Trade Body Added need styling

--- ai-trader/src/components/Trade/Trade.js
+++ ai-trader/src/components/Trade/Trade.js
@@ -17,7 +17,24 @@ const Trade =()=>{
                 </div>
             </Accordion.Header>
             <Accordion.Body>
-                
+                 {/* First line */}
+                <div className="acc-row">
+                    <span className="acc-left">Order #123456</span>
+                    <span className="acc-right">AAPL Corporation</span>
+                </div>
+                {/* Second line */}
+                <div className="acc-row">
+                    <span className="acc-left">Swap: $2.50</span>
+                    <span className="acc-center">Price: $180.00</span>
+                    <span className="acc-right">Stop Loss: $175.00</span>
+                </div>
+                {/* Third line */}
+                <div className="acc-row">
+                    <span className="acc-left">2024-06-14 10:30</span>
+                    <span className="acc-center">Price: $182.00</span>
+                    <span className="acc-right">Take Profit: $190.00</span>
+                </div>
+                                
             </Accordion.Body>
             </Accordion.Item>
         </Accordion>
