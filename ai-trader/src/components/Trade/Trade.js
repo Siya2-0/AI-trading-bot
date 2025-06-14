4293ecf782577cbdafc5f7b09d12c495faadd043
Siyabonga Mbuyisa <siyabongambuyisa7@gmail.com>
2025-06-14 20:21:06 +0200

    Trader Header Added
    Trader Header Added but styling not  done

--- ai-trader/src/components/Trade/Trade.js
+++ ai-trader/src/components/Trade/Trade.js
@@ -6,32 +6,22 @@ const Trade =()=>{
 
 
     return (
-    <Accordion defaultActiveKey="0">
-      <Accordion.Item eventKey="0">
-        <Accordion.Header>Accordion Item #1</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-      <Accordion.Item eventKey="1">
-        <Accordion.Header>Accordion Item #2</Accordion.Header>
-        <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
+    <div className='Temp-Trade-Container'>    
+        <Accordion defaultActiveKey="0" flush>
+            <Accordion.Item eventKey="0">
+            <Accordion.Header className="stock-header"> 
+                <div className="stock-row">
+                    <span className="stock-name">AAPL</span>
+                    <span className="stock-status">Buy 0.01</span>
+                    <span className="stock-winning">-R 1,200.00</span>
+                </div>
+            </Accordion.Header>
+            <Accordion.Body>
+                
+            </Accordion.Body>
+            </Accordion.Item>
+        </Accordion>
+    </div>
   );
 
 
