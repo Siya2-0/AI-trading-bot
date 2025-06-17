04277890c3950a7fdd98163202a24e06d0c80b39
Siyabonga Mbuyisa <siyabongambuyisa7@gmail.com>
2025-06-17 19:17:00 +0200

    className updated
    the following className have been added stock-status-sell and stock-status-buy
    
    stock-winning and stock-lossing

--- ai-trader/src/components/Trade/Trade.js
+++ ai-trader/src/components/Trade/Trade.js
@@ -12,27 +12,27 @@ const Trade =()=>{
             <Accordion.Header className="stock-header"> 
                 <div className="stock-row">
                     <span className="stock-name">AAPL</span>
-                    <span className="stock-status">Buy 0.01</span>
-                    <span className="stock-winning">-R 1,200.00</span>
+                    <span className="stock-status-buy">Buy 0.01</span>
+                    <span className="stock-losing">- 0.76</span>
                 </div>
             </Accordion.Header>
             <Accordion.Body>
                  {/* First line */}
                 <div className="acc-row">
-                    <span className="acc-left">Order #123456</span>
-                    <span className="acc-right">AAPL Corporation</span>
+                    <span className="acc-left">Order #1479175808</span>
+                    <span className="acc-right">Apple Inc Corporation</span>
                 </div>
                 {/* Second line */}
                 <div className="acc-row">
-                    <span className="acc-left">Swap: $2.50</span>
-                    <span className="acc-center">Price: $180.00</span>
-                    <span className="acc-right">Stop Loss: $175.00</span>
+                    <span className="acc-left">Swap: 0.50</span>
+                    <span className="acc-center">0.64687</span>
+                    <span className="acc-right">S/L: 0.65703</span>
                 </div>
                 {/* Third line */}
                 <div className="acc-row">
                     <span className="acc-left">2024-06-14 10:30</span>
-                    <span className="acc-center">Price: $182.00</span>
-                    <span className="acc-right">Take Profit: $190.00</span>
+                    <span className="acc-center">0.64763</span>
+                    <span className="acc-right">T/P: 0.63667</span>
                 </div>
                                 
             </Accordion.Body>
