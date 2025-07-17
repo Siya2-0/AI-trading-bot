229240dadec66e9698ba7a1d4a41c5659195ac4a
Siyabonga Mbuyisa <siyabongambuyisa7@gmail.com>
2025-07-17 20:35:39 +0200

    Responsive  Summary Box Added Now require Styling
    -Balanace(ROI)
    Buy Trade (SuccessRate)
    Sell Teade (SucessRate)
    General Success Rate

---
+++ ai-trader/src/components/MpBox/MpBox.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MpBox.css';
+
+const MpBox = () => {
+
+
+    return (
+        <div className="box">Box 1</div>
+
+    )
+
+};
+
+export default MpBox;
--- ai-trader/src/pages/ModelPerformance/ModelPerformance.js
+++ ai-trader/src/pages/ModelPerformance/ModelPerformance.js
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 import './ModelPerformance.css';
+import MpBox from '../../components/MpBox/MpBox'
+
 
 
 
@@ -8,6 +10,20 @@ const HistoryPage = () => {
 
     return (
         <div className='MP-Background' >
+            <div className="MP-dashboard">
+                {/* First container with 3 rectangular boxes */}
+                <div className="boxes-container">
+                    <MpBox />
+                    <MpBox />
+                    <MpBox />
+                    <MpBox />
+                </div>
+                
+                {/* Second container (you can add content here later) */}
+                <div className="second-container">
+                    {/* Content goes here */}
+                </div>
+            </div>
         
         </div>
     )
