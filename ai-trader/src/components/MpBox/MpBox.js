50bfbbcc95c9ad9de0daa1f4958c416b264193d7
Siyabonga Mbuyisa <siyabongambuyisa7@gmail.com>
2025-07-18 20:07:12 +0200

    Data Added to Box But requires styling

--- ai-trader/src/components/MpBox/MpBox.js
+++ ai-trader/src/components/MpBox/MpBox.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import './MpBox.css';
+import { IoIosWallet } from "react-icons/io";
 
 const MpBox = () => {
 
 
     return (
-        <div className="box">Box 1</div>
+        <div className="box">
+             <div className="balance-container">
+                <IoIosWallet className="wallet-icon" />
+                <div className="balance-text">
+                    <div className="balance-title">Total Balance</div>
+                    <div className="balance-change positive">+2.7%</div>
+                </div>
+                <div className="balance-amount">R125 8569.98</div>
+                <div className="balance-updated">Last updated: 15 July 2023, 10:30 AM</div>
+          </div>
+        
+        
+        </div>
 
     )
 
