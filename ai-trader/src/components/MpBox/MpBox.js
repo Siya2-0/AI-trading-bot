32149277f5c33fc1efcbb3565612d927287c1298
Siyabonga Mbuyisa <siyabongambuyisa7@gmail.com>
2025-07-19 19:34:22 +0200

    Styling added to Boxes
    content is  flex-direction: column
    rounded boarder added
    More styling may be needed

--- ai-trader/src/components/MpBox/MpBox.js
+++ ai-trader/src/components/MpBox/MpBox.js
@@ -7,16 +7,16 @@ const MpBox = () => {
 
     return (
         <div className="box">
-             <div className="balance-container">
+            <div className="balance-header">
                 <IoIosWallet className="wallet-icon" />
                 <div className="balance-text">
-                    <div className="balance-title">Total Balance</div>
+                    <div className="balance-title">Total Account Balance</div>
                     <div className="balance-change positive">+2.7%</div>
                 </div>
-                <div className="balance-amount">R125 8569.98</div>
-                <div className="balance-updated">Last updated: 15 July 2023, 10:30 AM</div>
-          </div>
-        
+                
+            </div>
+            <div className="balance-amount">R125 8569.98</div>
+            <div className="balance-updated">Last updated: 15 July 2023, 10:30 AM</div>
         
         </div>
 
